e5f927fd58195ef31a6ab3933b06353002bd678f
admin <admin@admins-MacBook-Pro.local>
2025-04-21 02:16:27 +0100

    service URL updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const serviceUrl = ""
+const serviceUrl = "/choreo-apis/django-react-notesapp/backend/v1"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL:serviceUrl
